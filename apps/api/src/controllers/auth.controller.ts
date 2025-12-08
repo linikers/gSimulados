2f4c52f1852217f0222397c4fb8a7574f43eae6f
linikers <linikers@hotmail.com>
2025-12-08 15:02:27 -0300

    test ok

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { AuthService } from "../../services/auth/auth.service";
+import { AuthService } from "../services/auth/auth.service";
+// import { AuthService } from "../../services/auth/auth.service";
 
 export class AuthController {
   static async register(req: Request, res: Response) {
