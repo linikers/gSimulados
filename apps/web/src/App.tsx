c6dd6f9bc7cc5e2a110bb40e6d056ef65b8a3045
linikers <linikers@hotmail.com>
2025-12-08 13:56:14 -0300

    fix de tipagem e gerenciamento de login

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { LoginPage } from "./pages/auth/Login";
 import { RegisterPage } from "./pages/auth/Register";
