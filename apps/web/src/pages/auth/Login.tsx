b9f9543ba8214d22511692ad14c242d74c9461f6
linikers <linikers@hotmail.com>
2026-01-27 14:16:46 -0300

    navLogin

--- apps/web/src/pages/auth/Login.tsx
+++ apps/web/src/pages/auth/Login.tsx
@@ -10,6 +10,7 @@ import {
   Paper,
 } from "@mui/material";
 import { useNavigate, Link } from "react-router-dom";
+import { Navbar } from "src/components/public/Navbar";
 
 export function LoginPage() {
   const { signIn } = useAuth();
@@ -23,6 +24,7 @@ export function LoginPage() {
     try {
       await signIn({ email, password });
       navigate("/dashboard");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       const msg = error.response?.data?.error || "Erro ao fazer login";
       showToast(msg, "error");
@@ -30,50 +32,53 @@ export function LoginPage() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper
-        elevation={3}
-        sx={{
-          mt: 8,
-          p: 4,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Senha"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Entrar
-          </Button>
-          <Link to="/register">Não tem conta? Cadastre-se</Link>
-        </Box>
-      </Paper>
-    </Container>
+    <>
+      <Navbar />
+      <Container component="main" maxWidth="xs">
+        <Paper
+          elevation={3}
+          sx={{
+            mt: 14,
+            p: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Entrar
+            </Button>
+            <Link to="/register">Não tem conta? Cadastre-se</Link>
+          </Box>
+        </Paper>
+      </Container>
+    </>
   );
 }
